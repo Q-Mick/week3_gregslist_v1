6cc4aec8b0d4235daafbc4b174e6c75076df87ce
Q-Mick <quentinmick@protonmail.com>
2023-05-10 17:03:56 -0600

    init

---
+++ app/Controllers/HousesController.js
@@ -0,0 +1,18 @@
+import { appState } from "../AppState.js";
+import { Car } from "../Models/Car.js";
+import { carsService } from "../Services/CarsService.js";
+import { getFormData } from "../Utils/FormHandler.js";
+import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
+
+
+
+export class HousesController {
+  constructor() {
+      console.log('hello from the HouseController controller');
+
+  
+  }
+
+
+}
